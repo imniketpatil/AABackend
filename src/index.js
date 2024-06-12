0ca1ef68515f75d11fb3bc004a9fcf66f2f0709f
Niket Patil <patilniket41@gmail.com>
2024-06-12 23:35:33 +0530

    Done Api Response and Error Handling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config();
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`SERVER IS RUNNING AT PORT : ${process.env.PORT}`);
+    });
+  })
+
+  .catch((error) => {
+    console.log("\n\nMONGO DB CONNECTION FAILED !!! \n\n", error);
+  });
